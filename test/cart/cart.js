85ddf863b84daebcba2a999dac60df1a87db5e67
Gabriel Bull <me@gabrielbull.com>
2016-03-22 18:35:22 -0400

    Fixed unit tests

--- test/cart/cart.js
+++ test/cart/cart.js
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
-import store from '../../src/app';
-import {defaultTax} from '../../src/items/item';
-import * as cart from '../../src/cart/cart';
+import store from '../../src/index';
+import {defaultItem} from '../../src/items/item';
 
 describe('Cart', () => {
   it('add item to cart', () => {
-    cart.addItem({...defaultTax});
-    expect(store.getState()['cart'].items.length).to.equal(1);
+    store.cart.addItem({...defaultItem});
+    expect(store.cart.items.length).to.equal(1);
   });
 
   it('remove item from cart', () => {
-    expect(store.getState()['cart'].items.length).to.equal(1);
-    cart.removeItem(0);
-    expect(store.getState()['cart'].items.length).to.equal(0);
+    expect(store.cart.items.length).to.equal(1);
+    store.cart.removeItem(0);
+    expect(store.cart.items.length).to.equal(0);
   });
 
   it('change cart item quantity', () => {
-    cart.addItem({...defaultTax});
-    cart.changeItemQuantity(0, 5);
-    expect(store.getState()['cart'].items[0].quantity).to.equal(5);
+    store.cart.addItem({...defaultItem});
+    store.cart.changeItemQuantity(0, 5);
+    expect(store.cart.items[0].quantity).to.equal(5);
   });
 });
