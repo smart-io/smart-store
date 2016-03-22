687b66d83d011d95afff8a915461b5bf4294b46f
Gabriel Bull <me@gabrielbull.com>
2016-03-22 17:54:15 -0400

    Fixed default values of order

--- src/order/order.js
+++ src/order/order.js
@@ -3,9 +3,9 @@ import * as addressActions from './address/address-actions';
 import * as itemsActions from '../items/items-actions';
 import * as customerActions from './customer/customer-actions';
 import * as cardActions from './card/card-actions';
-import address from './address/address';
-import customer from './customer/customer';
-import card from './card/card';
+import {defaultAddress} from './address/address';
+import {defaultCustomer} from './customer/customer';
+import {defaultCard} from './card/card';
 
 export const update = order => orderActions.updateOrder(order);
 export const validate = order => orderActions.validateOrder(order);
@@ -27,20 +27,13 @@ export const defaultOrder = {
   status: null,
   tracking_number: null,
   currency: 'USD',
-  shipping_address: { ...address },
-  billing_address: { ...address },
-  customer: { ...customer },
-  card: { ...card },
+  shipping_address: { ...defaultAddress },
+  billing_address: { ...defaultAddress },
+  customer: { ...defaultCustomer },
+  card: { ...defaultCard },
   items: [],
   subtotal: 0,
   fees: [],
   taxes: [],
   total: 0
 };
-
-//class Order {
-/*
- place() {
- return dispatch(Actions.placeOrder());
- }*/
-//}
